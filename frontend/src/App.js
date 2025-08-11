fbec045a1edf0578cf94cc04ef04b6155826718c
Christopher Adams <chrismichaeladams@gmail.com>
2025-08-11 11:52:33 -0400

    Add Search handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
+import { useState } from 'react';
+
+
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    console.log(searchTerm)
+  }
+
   return (
-    <div className="App">
+    <div>
       <Header title="Image Gallery" />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearchSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+  backgroundColor: '#eeeeee',
+};
 
 const Header = ({title}) => {
     return(
-      <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-      </Navbar>
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
     )
 }
 
 export default Header
+
